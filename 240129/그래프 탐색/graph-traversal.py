ecefb85666379933dca8d902b91e5621df4a8085
joho54 <70460688+joho54@users.noreply.github.com>
2024-01-29 12:47:21 +0900

    Title: 그래프 탐색, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240129/그래프 탐색/graph-traversal.py
@@ -0,0 +1,25 @@
+#인접 행렬 사용
+n, m = tuple(map(int, input().split()))
+edges = [
+    tuple(map(int, input().split()))
+    for _ in range(m)
+]
+
+visited = [False]*(n+1)
+global ans
+ans = 0
+def dfs(vertex):
+    global ans
+    for curr_v in range(1, n+1):
+        if graph[vertex][curr_v] and not visited[curr_v]:
+            visited[curr_v] = True
+            dfs(curr_v)
+            ans += 1
+
+#graph record
+graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
+for x, y in edges:
+    graph[x][y] = 1
+root = 1
+dfs(root)
+print(ans)
