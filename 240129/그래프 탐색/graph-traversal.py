2a7d51fb6883dba5bcfa7bef836861f2bfbd6a04
joho54 <70460688+joho54@users.noreply.github.com>
2024-01-29 13:10:21 +0900

    Title: 그래프 탐색, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240129/그래프 탐색/graph-traversal.py
+++ 240129/그래프 탐색/graph-traversal.py
@@ -17,14 +17,27 @@ def dfs(vertex):
             # print("entering:", curr_v)
             dfs(curr_v)
 
+def dfs_byAdjacancyList(vertex):
+    global ans
+    for curr_v in range(1, n+1):
+        if not visited[curr_v]:
+            visited[curr_v] = True
+            ans += 1
+            # print("entering:", curr_v)
+            dfs_byAdjacancyList(curr_v)
+
 #graph record
 graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
+adjacancyList = [[] for _ in range(n+1)]
 for x, y in edges:
     graph[x][y] = 1
     graph[y][x] = 1
+for x, y in edges:
+    graph[x].append(y)
+    graph[y].append(x)
 # for g in graph:
 #     print(g)
 root = 1
 visited[root]= True
-dfs(root)
+dfs_byAdjacancyList(root)
 print(ans)
