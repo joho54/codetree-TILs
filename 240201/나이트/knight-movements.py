3399f9ec024c88d70eb9348d2308dcb69e61a0ab
joho54 <70460688+joho54@users.noreply.github.com>
2024-02-01 12:43:00 +0900

    Title: 나이트, Time: 181ms, Memory: 27MB, Status: Passed - CodeTree

--- 240201/나이트/knight-movements.py
+++ 240201/나이트/knight-movements.py
@@ -26,10 +26,9 @@ def canGo(x, y):
     if inRange(x, y) and not visited[x][y]: 
         return True
     return False
-found = False
+
 
 def bfs():
-    global found
     dxs = [-1, -2, -2, -1, 1, 2, 2, 1]
     dys = [-2, -1, 1, 2, 2, 1, -1, -2]
     
@@ -40,12 +39,10 @@ def bfs():
             if canGo(new_x,new_y):
                 visited[new_x][new_y] = True
                 push(new_x, new_y, step[x][y]+1)
-                if new_x == r2 and new_y == c2:
-                    found = True
 
 push(r1,c1, 0)
 bfs()
 
-if not found:
-    print(-1)
-else: print(step[r2][c2])
+if step[r2][c2] != 0 or (r1, c1) == (r2, c2):
+    print(step[r2][c2])
+else: print(-1)
