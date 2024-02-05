533ce1d495f2215bf99690a5e0b7175111edfeb1
joho54 <70460688+joho54@users.noreply.github.com>
2024-02-05 19:31:57 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 150ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240205/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -0,0 +1,56 @@
+#input
+n = int(input())
+arr = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+dp = [[[0, 0, 0] for _ in range(n)] for _ in range(n)]
+#start point: 0, 0
+dp[0][0] = [0, arr[0][0], arr[0][0]]
+
+#set dp
+
+
+#vertical
+for i in range(1, n):
+    minValTmp = dp[i-1][0][1]
+    maxValTmp = dp[i-1][0][2]
+    if minValTmp > arr[i][0]:
+        minValTmp = arr[i][0]
+    if maxValTmp < arr[i][0]:
+        maxValTmp = arr[i][0]
+    ansVal = maxValTmp - minValTmp
+    dp[i][0] = [ansVal, minValTmp, maxValTmp] 
+
+#horizontal
+for j in range(1, n):
+    minValTmp = dp[0][j-1][1]
+    maxValTmp = dp[0][j-1][2]
+    if minValTmp > arr[0][j]:
+        minValTmp = arr[0][j]
+    if maxValTmp < arr[0][j]:
+        maxValTmp = arr[0][j]
+    ansVal = maxValTmp - minValTmp
+    dp[0][j] = [ansVal, minValTmp, maxValTmp] 
+
+
+def calc(ls, val):
+    v, minVal, maxVal = ls
+    #update min, max
+    if minVal > val: minVal = val
+    if maxVal < val: maxVal = val
+    return (maxVal - minVal, minVal, maxVal)
+
+
+for i in range(1, n):
+    for j in range(1, n):
+        val1, minVal1, maxVal1 = calc(dp[i-1][j], arr[i][j])
+        val2, minVal2, maxVal2 = calc(dp[i][j-1], arr[i][j])
+        dp[i][j] = min([val1, minVal1, maxVal1],[val2, minVal2, maxVal2])
+
+# for e in dp:
+#     print(e)
+
+
+print(dp[n-1][n-1][0])
