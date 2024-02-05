243c9707fb298f3d5ddb1e992220a5ce092f8366
joho54 <70460688+joho54@users.noreply.github.com>
2024-02-05 17:47:16 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 885ms, Memory: 40MB, Status: Wrong Answer - CodeTree

--- 240205/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240205/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,40 +1,54 @@
-#input
+# 변수 선언 및 입력:
 n = int(input())
-arr = [
-    tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+dp = [
+    [0] * n
     for _ in range(n)
 ]
 
-arrDetailed = []
-
-for i in range(n):
-    for j in range(n):
-        arrDetailed.append((arr[i][j], i, j))
-
-#(val, i, j)
-
-arrDetailed.sort()
-
-
-dp = [[1 for _ in range(n)]for _ in range(n)]
+cells = []
 ans = 0
-dxs = [-1, 0, 1, 0]
-dys = [0, -1, 0, 1]
 
-def inRange(x,y):
-    return x >= 0 and x < n and y >= 0 and y < n
 
-#dp
-for details in arrDetailed:
-    v, i, j = details
-    for dx, dy in zip(dxs, dys):
-        new_i, new_j = i+dx, j+dy
-        if inRange(new_i, new_j) and arr[new_i][new_j] > arr[i][j]:
-            dp[new_i][new_j] = max(dp[new_i][new_j], dp[i][j]+1)
-            
-    #if we have smaller neighbor 
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+   
+# 각 칸에 적혀있는 정수값 기준으로
+# 오름차순이 되도록 칸의 위치들을 정렬합니다.
+# 편하게 정렬하기 위해
+# (칸에 적혀있는 값, 행 위치, 열 위치) 순으로 넣어줍니다.
 for i in range(n):
     for j in range(n):
-        ans = max(ans, dp[i][j])
+        cells.append((grid[i][j], i, j))
 
-print(ans)
+# 오름차순으로 정렬을 진행합니다.
+cells.sort()
+
+# 처음 DP 값들은 전부 1로 초기화해줍니다. (해당 칸에서 시작하는 경우)
+for i in range(n):
+    for j in range(n):
+        dp[i][j] = 1
+
+# 정수값이 작은 칸부터 순서대로 보며
+# 4방향에 대해 dp 값을 갱신해줍니다.
+for _, x, y in cells:
+    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+    # 인접한 4개의 칸에 대해 갱신을 진행합니다.
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and grid[nx][ny] > grid[x][y]:
+            dp[nx][ny] = max(dp[nx][ny], dp[x][y] + 1)
+
+# 전체 수들 중 최댓값을 찾습니다.
+# for i in range(n):
+#     for j in range(n):
+#         ans = max(ans, dp[i][j])
+
+
+# print(ans)
+print(max(max(dp)))
