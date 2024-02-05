0c6f0ccee0a6efe6437a9e07a9da8a659ff0f4a3
joho54 <70460688+joho54@users.noreply.github.com>
2024-02-05 17:16:52 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 932ms, Memory: 40MB, Status: Wrong Answer - CodeTree

--- 240205/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240205/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -5,12 +5,16 @@ arr = [
     for _ in range(n)
 ]
 
-arrOneDimen = []
+arrDetailed = []
+
 for i in range(n):
     for j in range(n):
-        if not arr[i][j] in arrOneDimen:
-            arrOneDimen.append(arr[i][j])
-arrOneDimen.sort()
+        arrDetailed.append((arr[i][j], i, j))
+
+#(val, i, j)
+
+arrDetailed.sort()
+
 
 dp = [[-1 for _ in range(n)]for _ in range(n)]
 
@@ -21,20 +25,19 @@ def inRange(x,y):
     return x >= 0 and x < n and y >= 0 and y < n
 
 #dp
-for val in arrOneDimen:
-    for i in range(n):
-        for j in range(n):
-            if arr[i][j] == val:
-                dps = []
-                for dx, dy in zip(dxs, dys):
-                    new_i, new_j = i+dx, j+dy
-                    if inRange(new_i, new_j) and arr[new_i][new_j] < arr[i][j]:
-                        tmp = dp[new_i][new_j] 
-                        dps.append(tmp)
-                #if we have smaller neighbor 
-                if len(dps) > 0:
-                    dp[i][j] = max(dps)+1
-                #otherwise
-                else: dp[i][j] = 1
+for details in arrDetailed:
+    v, i, j = details
+
+    dps = []
+    for dx, dy in zip(dxs, dys):
+        new_i, new_j = i+dx, j+dy
+        if inRange(new_i, new_j) and arr[new_i][new_j] < arr[i][j]:
+            tmp = dp[new_i][new_j] 
+            dps.append(tmp)
+    #if we have smaller neighbor 
+    if len(dps) > 0:
+        dp[i][j] = max(dps)+1
+    #otherwise
+    else: dp[i][j] = 1
                 
 print(max(max(dp)))
