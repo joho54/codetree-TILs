ec913eeeba576140c6351833dd1bdd8dd3449be2
joho54 <70460688+joho54@users.noreply.github.com>
2024-02-05 15:50:16 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 169ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240205/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -0,0 +1,31 @@
+#input
+n = int(input())
+arr = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+dp = [[0 for _ in range(n)] for _ in range(n)]
+#start point: 0, 0
+dp[0][0] = arr[0][0]
+
+#set dp
+
+#vertical
+for i in range(1, n):
+    dp[i][0] = min(arr[i][0], dp[i-1][0])
+
+#horizontal
+for j in range(1, n):
+    dp[0][j] = min(arr[0][j], dp[0][j-1])
+
+
+
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = max(min(arr[i][j], dp[i-1][j]), min(arr[i][j], dp[i][j-1]))
+# for e in dp:
+#     print(e)
+
+
+print(dp[n-1][n-1])
