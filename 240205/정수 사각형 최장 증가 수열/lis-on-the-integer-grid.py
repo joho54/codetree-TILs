7c3753ee0c46754197e87c7c30fd4c7332c9fefa
joho54 <70460688+joho54@users.noreply.github.com>
2024-02-05 16:39:20 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 111ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240205/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -0,0 +1,37 @@
+import sys
+MIN_INT = -sys.maxsize
+
+#input
+n = int(input())
+arr = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+dp = [[-1 for _ in range(n)]for _ in range(n)]
+
+dxs = [-1, 0, 1, 0]
+dys = [0, -1, 0, 1]
+
+def inRange(x,y):
+    return x >= 0 and x < n and y >= 0 and y < n
+
+minVal, maxVal = min(min(arr)), max(max(arr))
+print(minVal, maxVal)
+#dp
+for val in range(minVal, maxVal+1):
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] == val:
+                dps = []
+                for dx, dy in zip(dxs, dys):
+                    new_i, new_j = i+dx, j+dy
+                    if inRange(new_i, new_j) and arr[new_i][new_j] < arr[i][j]:
+                        tmp = dp[new_i][new_j] 
+                        dps.append(tmp)
+                #if we have smaller neighbor 
+                if len(dps) > 0:
+                    dp[i][j] = max(dps)+1
+                #otherwise
+                else: dp[i][j] = 1
+print(max(max(dp)))
