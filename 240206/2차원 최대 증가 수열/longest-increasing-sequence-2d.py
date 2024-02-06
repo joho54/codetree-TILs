81bbb2789da7fa38916b3b0e96bd43cb3dd61044
joho54 <70460688+joho54@users.noreply.github.com>
2024-02-06 16:00:29 +0900

    Title: 2차원 최대 증가 수열, Time: 162ms, Memory: 28MB, Status: Passed - CodeTree

--- 240206/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240206/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -19,11 +19,11 @@ def initialize():
             dp[i][j] = MIN_INT
 initialize()
 
-for i in range(n):
-    dp[i][0] = 0
+# for i in range(n):
+#     dp[i][0] = 0
     
-for j in range(m):
-    dp[0][j] = 0
+# for j in range(m):
+#     dp[0][j] = 0
 
 dp[0][0] = 1
 
