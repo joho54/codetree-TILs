8f8f519824abc7688c87f3cedcf40b6520da317c
joho54 <70460688+joho54@users.noreply.github.com>
2024-02-06 17:24:46 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 151ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240206/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 240206/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -9,7 +9,7 @@ dp = [1 for _ in range(n)]
 
 dp[0] = 1
 
-for i in range(n):
+for i in range(1, n):
     for j in range(0, i):
         if lines[j][1] < lines[i][0]:
             dp[i] = max(dp[j]+1, dp[i])
