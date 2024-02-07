b055d68442e5ee95371882b4dd579f514f7e0b16
joho54 <70460688+joho54@users.noreply.github.com>
2024-02-07 16:12:57 +0900

    Title: 부분 수열의 합이 m, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240207/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
@@ -0,0 +1,28 @@
+import sys
+
+MAX_INT = sys.maxsize
+
+#j, m : dp index
+#i, n: A index
+#input
+n, m = tuple(map(int, input().split()))
+
+A = tuple(map(int, input().split()))
+
+#dp: dp len = 0~~m
+dp = [MAX_INT for _ in range(m+1)]
+dp[0] = 0
+
+#initialize dp
+for i in range(1, n):  
+    for j in range(m, -1, -1): 
+        #same condition with finding max
+        if i >= dp[j-A[i]]:
+            if dp[j-A[i]] == MAX_INT:
+                continue
+            dp[j] = min(dp[j], dp[j-A[i]]+1)
+
+ans = dp[m]
+if ans == MAX_INT:
+    ans = -1
+print(ans)
