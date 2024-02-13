53f5f152453c2d361a4ecc7d815b1f5c31c0e792
joho54 <70460688+joho54@users.noreply.github.com>
2024-02-13 19:53:05 +0900

    Title: 두 수의 합, Time: 181ms, Memory: 36MB, Status: Passed - CodeTree

--- 240213/두 수의 합/sum-of-two-num.py
+++ 240213/두 수의 합/sum-of-two-num.py
@@ -7,16 +7,18 @@ count = dict()
 ans = 0
 # 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
 for elem in arr:
+        
+    diff = k - elem
+    
+    # 가능한 모든 쌍의 수를 세어줍니다.
+    if diff in count:
+        ans += count[diff]
+    #중복을 피하기 위해 여기 있음. 인덱스가 증가하면서 현재 요소를 포함시켜줌. 그림으로 그리면
+    #거꾸로 탐색하는 것과 같음.
     # 현재 숫자의 개수를 하나 증가시켜줍니다.
     if elem in count:
         count[elem] += 1
     else:
         count[elem] = 1
-        
-    diff = k - elem
-    # 가능한 모든 쌍의 수를 세어줍니다.
-    if diff in count:
-        ans += count[diff]
-
 
 print(ans)
