a072f6ae3c879fc07c513959a4315cebb65f8bbb
joho54 <70460688+joho54@users.noreply.github.com>
2024-02-14 17:43:41 +0900

    Title: 대칭 차집합, Time: 268ms, Memory: 61MB, Status: Passed - CodeTree

---
+++ 240214/대칭 차집합/symmetric-difference-set.py
@@ -0,0 +1,14 @@
+a, b = tuple(map(int, input().split()))
+A = tuple(map(int, input().split()))
+B = tuple(map(int, input().split()))
+
+aSet = set(A)
+bSet = set(B)
+ans = 0
+for e in A:
+    if e not in bSet:
+        ans += 1
+for e in B:
+    if e not in aSet:
+        ans += 1
+print(ans)
