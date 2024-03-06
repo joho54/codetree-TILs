6efd62e249fe0b625053f11ed3c33c34c20c1ae3
joho54 <70460688+joho54@users.noreply.github.com>
2024-03-06 17:01:10 +0900

    Title: 정수 선언하고 뺄셈, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240306/정수 선언하고 뺄셈/define-numbers-and-substract.java
+++ 240306/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        int a, b;
+        int a, b, c;
         a = 97;
         b = 13;
         c = a-b
