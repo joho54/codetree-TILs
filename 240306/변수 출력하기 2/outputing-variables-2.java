4015ef5a9a8a704887b7e08dc70e34862520868f
joho54 <70460688+joho54@users.noreply.github.com>
2024-03-06 17:08:31 +0900

    Title: 변수 출력하기 2, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240306/변수 출력하기 2/outputing-variables-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a;
+        char c;
+        a = 3;
+        c = 'C';
+        System.out.println(c + "!.....!" + a);
+
+    }
+}
