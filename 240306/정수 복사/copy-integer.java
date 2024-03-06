cda9eb776456d94bc9dc6533de3d427cb93ac0d2
joho54 <70460688+joho54@users.noreply.github.com>
2024-03-06 17:32:10 +0900

    Title: 정수 복사, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 240306/정수 복사/copy-integer.java
+++ 240306/정수 복사/copy-integer.java
@@ -6,7 +6,7 @@ public class Main {
         b = 4;
         b = a;
         int c = a*b;
-        System.out.println(a, b);
+        System.out.println(a + " " + b);
         System.out.println(c);
 
     }
