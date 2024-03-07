009556a27314700a409e425d4f5fb56c53b7811a
joho54 <70460688+joho54@users.noreply.github.com>
2024-03-07 14:27:25 +0900

    Title: 정수 세 개 입력받아 출력, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240307/정수 세 개 입력받아 출력/take-three-integers-and-output.java
+++ 240307/정수 세 개 입력받아 출력/take-three-integers-and-output.java
@@ -7,9 +7,7 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        System.out.printf("%d %d %d", a, b, c);
 
     }
 }
