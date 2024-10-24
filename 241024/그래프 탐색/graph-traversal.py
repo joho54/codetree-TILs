c7be48c0258f5aa8f5a9301f64e93df4ec383f18
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2024-10-24 17:10:18 +0900

    Title: 그래프 탐색, Time: 318ms, Memory: 33MB, Status: Passed - CodeTree

--- 241024/그래프 탐색/graph-traversal.py
+++ 241024/그래프 탐색/graph-traversal.py
@@ -2,23 +2,35 @@ n, m = tuple(map(int, input().split()))
 
 #adjacency matrix
 graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
+graph2 = [[] for _ in range(n+1)]
 visited = [0 for _ in range(n+1)]
+visited2 = [0 for _ in range(n+1)]
+
 
 for _ in range(m):
     x, y = tuple(map(int, input().split()))
     graph[x][y] = 1
     graph[y][x] = 1
 
-def dfs(vertex):
+    graph2[x].append(y)
+    graph2[y].append(x)
+
+
+def dfs_adjacent_matrix(vertex):
     # if it's not visted and edge exist
     for curr_v in range(1, n+1):
         if graph[vertex][curr_v] == 1 and visited[curr_v] == 0:
             visited[curr_v] = 1
-            dfs(curr_v)
-            
+            dfs_adjacent_matrix(curr_v)
 
+def dfs_adjacent_list(vertex):
+    for curr_v in graph2[vertex]:
+        if visited2[curr_v] == 0:
+            visited2[curr_v] = 1
+            dfs_adjacent_list(curr_v)
+    
+    
 
+dfs_adjacent_list(1)
 
-dfs(1)
-
-print(sum(visited)-1 if sum(visited)-1 >= 0 else 0)
+print(sum(visited2)-1 if sum(visited2)-1 >= 0 else 0)
