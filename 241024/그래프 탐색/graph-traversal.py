be628be7fa03b488bccfa77ce64c9a46dbc132ac
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2024-10-24 16:47:23 +0900

    Title: 그래프 탐색, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241024/그래프 탐색/graph-traversal.py
+++ 241024/그래프 탐색/graph-traversal.py
@@ -1,25 +1,24 @@
 n, m = tuple(map(int, input().split()))
 
 #adjacency matrix
-graph = [[0] * (n+1)] *(n+1)
-visited = [False] * (n+1)
+graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
+visited = [0 for _ in range(n+1)]
 
 for _ in range(m):
     x, y = tuple(map(int, input().split()))
     graph[x][y] = 1
-
-
+    graph[y][x] = 1
 
 def dfs(vertex):
     # if it's not visted and edge exist
-
     for curr_v in range(1, n+1):
-        if graph[vertex][curr_v] == 1 and not visited[vertex]:
-            visited[vertex] = True
+        if graph[vertex][curr_v] == 1 and visited[curr_v] == 0:
+            visited[curr_v] = 1
             dfs(curr_v)
+            
 
 
 
 dfs(1)
 
-print(sum(visited))
+print(sum(visited)-1)
