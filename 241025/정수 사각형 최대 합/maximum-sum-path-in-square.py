bf90ce37e0c5dce4982649d6d462e3accd8c69d4
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2024-10-25 16:58:56 +0900

    Title: 정수 사각형 최대 합, Time: 163ms, Memory: 25MB, Status: Passed - CodeTree

--- 241025/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 241025/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -19,7 +19,7 @@ dp = [
 def initialize():
     # vertical down
     dp[0][0] = grid[0][0]
-    for i in range(n):
+    for i in range(1, n):
         dp[i][0] = dp[i-1][0] + grid[i][0]
         dp[0][i] = dp[0][i-1] + grid[0][i]
 
