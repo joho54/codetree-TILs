85b145eaa429ac10d1d75c484b130c08ff72bbf3
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2024-10-30 14:27:27 +0900

    Title: 자리 차지하기, Time: 433ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 241030/자리 차지하기/take-place.java
+++ 241030/자리 차지하기/take-place.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static TreeSet<Integer> s = new TreeSet<>();
+
     public static TreeSet<Integer> seats = new TreeSet<>();
     public static Scanner sc = new Scanner(System.in);
     public static int n = sc.nextInt();
@@ -16,12 +16,11 @@ public class Main {
         // lower
         for(int i = 0; i < n; i++){
             a[i] = sc.nextInt();
-            s.add(a[i]);
             // 1. 규칙이 작은 사람을 먼저 꺼낸다.
             // 2. 규칙이 작은 사람이 앉을 수 있는 최대한 작은 자리에 앉힌다.
         }
         int seat_cnt = 0;
-        for(int a_i: s){
+        for(int a_i: a){
             
             if (seat_cnt > a_i) break;
             // seats[seat_cnt] = 1; // mark the seat.
@@ -35,6 +34,7 @@ public class Main {
             // seat treeset을 따로 만들어서 거기에다 a_i를 순서대로 집어넣고, 
             // queries를 따로 순서대로 돌면서 아니아니
             // 여기서 집어넣기 전에 a_i에 대해 lower 돌리고, 같으면 브레이크 걸면 되지..
+            // System.out.println(a_i + " so " + seat_cnt);
             seats.add(a_i);
         }
         System.out.println(seat_cnt);
