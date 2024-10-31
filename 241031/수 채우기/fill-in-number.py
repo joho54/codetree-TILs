949bfc6235e98817b820ea3675a720480df8eb95
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2024-10-31 15:47:14 +0900

    Title: 수 채우기, Time: 182ms, Memory: 25MB, Status: Passed - CodeTree

--- 241031/수 채우기/fill-in-number.py
+++ 241031/수 채우기/fill-in-number.py
@@ -12,10 +12,18 @@ dp = [MAX_INT for _ in range(n+1)]
 # dp[n] = n을 만들기 위해 필요한 최소 동전의 수
 # dp[n] = min(dp[n-2], dp[n-5])+1
 
-dp[2] = 1
-dp[5] = 1
+
+
+def initialize(n):
+    init_arr = [0 , MAX_INT, 1, MAX_INT, 2, 1] # 5
+    n = n if n < 6 else 6
+    for i in range(0, n): # 
+        dp[i] = init_arr[i]
+
+initialize(n)
 
 for i in range(6, n+1):
     dp[i] = min(dp[i-2], dp[i-5]) + 1
 
-print(dp[n])
+ans = dp[n] if dp[n] != MAX_INT else -1
+print(ans)
