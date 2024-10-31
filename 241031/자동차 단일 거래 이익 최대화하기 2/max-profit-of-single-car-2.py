f2a39c12b2613952e134189a41c87c5261f7be5c
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2024-10-31 16:35:02 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241031/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 241031/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -1,27 +1,45 @@
+# n = int(input())
+# arr = tuple(map(int, input().split()))
+
+# pre_sub = []
+
+# for i in range(1, n):
+#     pre_sub.append(arr[i] - arr[i-1])
+
+# # print(pre_sub)
+
+# ans = pre_sub[0]
+# fin = pre_sub[0]
+
+# for elem in pre_sub[1:]:
+#     if ans < 0:
+#         ans = elem
+#     else:
+#         ans += elem
+#     fin = max(ans, fin)
+#     # print(ans)
+
+# fin = fin if fin > 0 else 0
+
+# print(fin)
+
+# inplace한 방식으로 바꿔 풀어봐라.
+
 n = int(input())
 arr = tuple(map(int, input().split()))
 
-# prefix sub = 1 -8 1 3
-# 이거 아까 그 뭐냐 그거, 최대합 만드는 문제랑 같음
-
-pre_sub = []
+max_profit = 0
+min_price = arr[0]
 
 for i in range(1, n):
-    pre_sub.append(arr[i] - arr[i-1])
+    # updpate profit
+    profit = arr[i] - min_price
 
-# print(pre_sub)
+    if profit > max_profit:
+        max_profit = profit
+    
+    # update min price
+    if arr[i] < min_price:
+        min_price = arr[i]
 
-ans = pre_sub[0]
-fin = pre_sub[0]
-
-for elem in pre_sub[1:]:
-    if ans < 0:
-        ans = elem
-    else:
-        ans += elem
-    fin = max(ans, fin)
-    # print(ans)
-
-fin = fin if fin > 0 else 0
-
-print(fin)
+print(profit)
