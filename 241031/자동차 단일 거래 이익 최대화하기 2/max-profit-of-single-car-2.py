7c8c43c12379c3908f5206a3cefcac46d902a1f4
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2024-10-31 16:36:03 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 165ms, Memory: 33MB, Status: Passed - CodeTree

--- 241031/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 241031/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -42,6 +42,6 @@ for i in range(1, n):
     if arr[i] < min_price:
         min_price = arr[i]
 
-profit = profit if profit > 0 else 0
+ans = max_profit if max_profit > 0 else 0
 
-print(profit)
+print(ans)
