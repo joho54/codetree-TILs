fdf7c694b0a9ab1554cb0523f8ee95ae18951cd4
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2024-10-31 14:17:57 +0900

    Title: 숫자 합치기, Time: 5054ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 241031/숫자 합치기/merge-numbers.py
+++ 241031/숫자 합치기/merge-numbers.py
@@ -4,21 +4,19 @@
 n = int(input())
 nums = list(map(int, input().split()))
 
-nums.sort()
+# nums.sort()
 ans = 0
 
 while len(nums) > 1: # until there are 2 elements
-    new_num = nums[0] + nums[1]
-    ans += new_num
-    # 이진 정렬 어떻게 하더라? 포인터 하나 가지고, 인덱스를 계속 갱신 하는데
-    # 언제까지 갱신하지? 양쪽 값이 nums[idx] 사이가 될 때?
-    # 종료 조건은 그거고, idx 감소 조건, 증가 조건도 따로지. 증가, 감소는 각각 어떻게 하지?
-    # 감소: 기존 idx//2
-    # 증가: 기존 int(idx*(1.5))
-    #그냥 뮤식하게 소트하자..
-    del nums[0]
-    nums[0] = new_num
-    nums.sort()
+    minA = min(nums)
+    idxA = nums.index(minA)
+    del nums[idxA]
+    minB = min(nums)
+    idxB = nums.index(minB)
+    
+    nums[idxB] = minA + minB
+    ans += nums[idxB]
 
+    # print(minA, minB)
 
 print(ans)
