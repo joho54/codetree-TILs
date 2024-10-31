85470eec466a540c835209f5b9c222b2fd932b83
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2024-10-31 13:51:22 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 353ms, Memory: 33MB, Status: Passed - CodeTree

---
+++ 241031/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -0,0 +1,25 @@
+# fractional knapsack problem
+
+n, m = tuple(map(int, input().split()))
+jewls = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+jewls = sorted(jewls, key = lambda x: x[0]/x[1]) # order by reversed value
+
+weight_val = [(w, v/w) for w, v in jewls]
+# print(weight_val)
+
+ans = 0
+cap = m # 남은 용량
+
+for w, wv in weight_val:
+    if cap - w >= 0:
+        ans += w * wv
+        cap -= w
+    else:
+        ans += cap * wv
+        break
+
+print("{:.3f}".format(ans))
