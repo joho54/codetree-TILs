a58fc6cc2efb646551a530779da22f2674e8302e
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2024-10-31 13:10:32 +0900

    Title: 동전 더하기, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241031/동전 더하기/add-coins.py
@@ -0,0 +1,16 @@
+# 배수일 떄는 그냥 큰동전부터 담는게 이득인 그리디 문제
+
+# 입력
+n, k = tuple(map(int, input().split()))
+coins = [0 for _ in range(n)]
+for i in range(n):  
+    coins[i] = int(input())
+
+cnt = 0 # increment. 
+
+for coin in coins[-1::-1]:  # 각각 큰 동전에 대해 루프를 거꾸로
+    while k - coin >= 0:
+        k -= coin
+        cnt += 1
+
+print(cnt)
