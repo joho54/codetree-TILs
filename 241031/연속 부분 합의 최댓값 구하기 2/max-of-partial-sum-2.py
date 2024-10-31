03121dc02c023a45948257a07841bdf98738fee4
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2024-10-31 13:18:19 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241031/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -0,0 +1,16 @@
+# 음수가 되는 순간 끊기. 합을 계속 갱신하다가, 
+
+# 입력
+n = int(input())
+arr = tuple(map(int, input().split()))
+ans = 0
+final_ans = 0
+
+for a in arr:
+    if ans+a > 0:
+        ans += a
+    else: 
+        ans = 0
+    final_ans = max(ans, final_ans)
+
+print(final_ans)
