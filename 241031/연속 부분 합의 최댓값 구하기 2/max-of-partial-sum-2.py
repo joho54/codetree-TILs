af64d7e7b4c55274083ed476ad8a89460c2d2a20
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2024-10-31 13:29:14 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 160ms, Memory: 32MB, Status: Passed - CodeTree

--- 241031/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 241031/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -7,10 +7,11 @@ ans = arr[0]
 final_ans = arr[0]
 
 for a in arr[1:]:
-    if ans+a >= 0:
+    # 만약 지금까지의 합이 0보다 작으면 끊는게 이득이다. 끊을 때 이렇게밖에 못 끊는다.
+    if ans < 0: 
+        ans = a
+    else:
         ans += a
-    else: 
-        ans = 0
     final_ans = max(ans, final_ans)
 
 print(final_ans)
