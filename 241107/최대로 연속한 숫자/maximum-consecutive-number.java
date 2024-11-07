2d24e2b6f9b28ad6a3e9891f5de842fa49a99ed5
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2024-11-07 22:37:12 +0900

    Title: 최대로 연속한 숫자, Time: 1666ms, Memory: 42MB, Status: Wrong Answer - CodeTree

--- 241107/최대로 연속한 숫자/maximum-consecutive-number.java
+++ 241107/최대로 연속한 숫자/maximum-consecutive-number.java
@@ -34,22 +34,61 @@ public class Main {
         int m = sc.nextInt();
         s.add(n+1);
         s.add(-1);
-        int ans = -1;// where should i init backup?
-        int backup = -1;
         for(int j = 0 ; j < m; j++){ // what? /
             int del_num = sc.nextInt();
             s.add(del_num);
             // compoare
+            int mid = 0;
             int tmp1 = del_num - s.lower(del_num) - 1;
             int tmp2 = s.higher(del_num) - del_num - 1 ;
-            // 0 1 2 3 4 5 6 7 8
-            // 0 1 2   4 5 6 7 8 
-            // 0 1 2   4 5   7 8
-            int tmp = Math.max(tmp1, tmp2); // first compare occurs here
-            tmp = Math.max(tmp, backup);
-            System.out.println(tmp);
+            mid = Math.max(tmp1, tmp2);
+            // 
+            Integer left = s.lower(del_num);
+            if (left != null)
+            {
+                int tmp3 = 0;
+                int tmp4 = 0;
+                Integer sibal = s.lower(left);
+                if(sibal != null) tmp3 = left - sibal - 1;
+                else tmp3 = 0;
+                
+                // higher of left is del num
+                tmp4 = del_num - left - 1 ;
+                left = Math.max(tmp3, tmp4);
+            }
+            else left = 0;
 
-            backup = Math.min(tmp1, tmp2);
+            Integer right = s.higher(del_num);
+            if(right !=null)
+            {
+                int tmp5 = 0;
+                int tmp6 = 0;
+                tmp5 = right - del_num - 1;
+                Integer sibal2 = s.higher(right);
+                if (sibal2 != null) tmp6 = sibal2 - right - 1 ;
+                else tmp6 = 0;
+                right = Math.max(tmp5, tmp6);
+            }
+            else right = 0;
+            // T[i] = max lcl when the del_num is i, and look both side.
+            // 0 1 2 3 4 5 6 7 8 9 10 // 11
+            // 0 1 2   4 5 6 7 8 9 10 // 3 T[3] = 7
+            // 0 1 2   4 5   7 8 9 10 // 3 T[3] = 3 T[6] = 4 // what the fuc is differ from bruteforce.
+            // there are only 2 arrays which is affected by new del_num. lower and higher.
+            //   1 2   4 5.  7 8 9 10??????????????????
+            //  -1 3 6 9
+            // why the fuck should we use treeset than?
+            // -1 3 9
+            // -1 3 6 9
+            // -1 2  3 6 9
+            // array manage?
+            // peices = curr_m + 1
+            // 
+            // we are missing unchanged area lcn.
+            // 
+            int ans = Math.max(mid, left);
+            ans = Math.max(ans, right);
+            System.out.println(ans);
         }
 
     }
